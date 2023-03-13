a91c0824d57e351c9d8b5352a74d1a6ca813fe07
tapanhp <tapanhparmar@gmail.com>
2023-03-13 19:03:19 +0530

    Added results

--- simulator.py
+++ simulator.py
@@ -19,11 +19,11 @@ total_won_credit_by_user = 0
 
 slot_machine = SlotMachine(TOTAL_SLOTS_IN_MACHINE)
 
-for user in range(1):
+for user in range(1, 2):
 
     credit_manager = CreditManager()
     credit_manager.set_initial_credit()
-    print(f"Provided initial credit {credit_manager.credits} to user {user + 1}")
+    print(f"Provided initial credit {credit_manager.credits} to user {user}")
     total_pre_credit_by_house += credit_manager.credits
 
     while True:
@@ -31,7 +31,7 @@ for user in range(1):
             credit_manager.deduct_credit_for_roll()  # 1
             total_received_credit_by_house += CREDIT_CUT_FOR_SINGLE_ROLL
 
-            print(f"User {user + 1} lost {CREDIT_CUT_FOR_SINGLE_ROLL}, Now credit {credit_manager.credits}")
+            print(f"User {user} lost {CREDIT_CUT_FOR_SINGLE_ROLL}, Now credit {credit_manager.credits}")
 
             slot_machine.pull_lever()
             total_hands_played += 1
@@ -43,18 +43,19 @@ for user in range(1):
                     total_cheat_run += 1
                     print(f"CHEAT for user with {credit_manager.credits} credit :: Re-rolling, saved {won_credit}")
                     total_saved_by_re_rolling += won_credit
-                    # Pull lever should be called here
+                    # Pull lever should be called here, Right now I am ignoring it
                     continue
 
                 total_jackpots += 1
                 total_won_credit_by_user += won_credit
+                print(f"User {user} won Jackpot! with credit {won_credit}, previous credits {credit_manager.credits}")
                 # slot_machine.show_jackpot_value()
 
                 total_spent_credit_by_house += won_credit
                 credit_manager.change_credit_after_roll(won_credit)
 
         except CreditExhaustedError:
-            print(f"User {user + 1} has exhausted credit, continue with next")
+            print(f"User {user} has exhausted credit, continue with next")
             break
 
     print(f"Total user credits are {credit_manager.credits}")
@@ -71,4 +72,4 @@ print(f"Total hands played {total_hands_played} \n"
       f"Total Pre credit spent by house {total_pre_credit_by_house}\n"
       f"and lost {total_spent_credit_by_house}")
 
-print(f"Total won credit by jackpots {total_won_credit_by_user}")
+# print(f"Total won credit by jackpots {total_won_credit_by_user}")
